f8fe77ee3039496eb3d98f768237c1014f7f9641
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-26 15:05:18 -0500

    added sql command for part 1

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 ## Part 1: Test it with SQL
+SELECT
+    column_name, data_type
+FROM
+    information_schema.columns
+WHERE
+    table_schema = 'techjobs'
+        AND table_name = 'job'
 
 ## Part 2: Test it with SQL
 

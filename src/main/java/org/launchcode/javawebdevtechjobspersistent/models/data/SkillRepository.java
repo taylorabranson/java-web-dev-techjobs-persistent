1da6f4f33307cecf78f5965e20d5b72eed8bcd89
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-26 15:07:46 -0500

    initial commit

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
@@ -1,2 +1,9 @@
-package org.launchcode.javawebdevtechjobspersistent.models.data;public interface SkillRepository {
+package org.launchcode.javawebdevtechjobspersistent.models.data;
+
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkillRepository extends CrudRepository<Skill, Integer> {
 }

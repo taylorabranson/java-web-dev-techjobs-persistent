b3a50eaf4faf8e0d6d63af07e1d17d50bbc52019
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-26 18:48:19 -0500

    removed unneccesary import

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,7 +1,6 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity

fb5c202f8abb3e7785c724f5047d9ca8c528d3c6
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-26 15:11:42 -0500

    added @Entity, fields/validation, constructors, getter/setters

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location must not be blank")
+    @Size(min = 3, max = 80, message = "Location must be between 3 and 80 characters")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }

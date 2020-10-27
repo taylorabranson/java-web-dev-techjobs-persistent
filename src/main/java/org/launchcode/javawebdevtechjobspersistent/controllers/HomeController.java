8b2c75d6c8e52775e699fb2b51c3122870a096f1
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-27 14:54:39 -0500

    implemented displayViewJob

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -67,7 +67,15 @@ public class HomeController {
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
 
-        return "view";
+        Optional<Job> optJob = jobRepository.findById(jobId);
+        if (optJob.isPresent()) {
+            Job job = (Job) optJob.get();
+            model.addAttribute("job", job);
+            return "view";
+        } else {
+            return "redirect:../";
+        }
+
     }
 
 

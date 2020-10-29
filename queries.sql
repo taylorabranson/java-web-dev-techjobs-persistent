caeeecbfe5dd942b55bf1ff8138f32dc34bfa4f7
Taylor Branson <taylor.a.branson@gmail.com>
2020-10-28 19:28:55 -0500

    mysql query for part 4

--- queries.sql
+++ queries.sql
@@ -7,6 +7,7 @@ WHERE
     table_schema = 'techjobs'
         AND table_name = 'job'
 
+
 ## Part 2: Test it with SQL
 SELECT
     *
@@ -15,7 +16,16 @@ FROM
 WHERE
     location = 'St. Louis City'
 
+
 ## Part 3: Test it with SQL
 DROP TABLE `techjobs`.`job`;
 
+
 ## Part 4: Test it with SQL
+SELECT DISTINCT
+    name, description
+FROM
+    skill
+        INNER JOIN
+    job_skills ON skill.id = job_skills.skills_id
+ORDER BY name
